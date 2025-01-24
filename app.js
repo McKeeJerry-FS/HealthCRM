43223d579000a33f67f259e702409b22db371458
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-01-23 21:04:03 -0500

    finishing initial setup

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const ejs = require('ejs');
 
+const port = 3000;
+app.set('views', __dirname + './views');
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
