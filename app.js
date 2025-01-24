1e7c7b50940004574bf0bf71a5b17b9cc7b93534
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-01-24 15:32:29 -0500

    Updated the package.json file

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+  
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
