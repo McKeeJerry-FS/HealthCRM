924d0b0d2a6354146f851b40456556955f256f13
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-01-25 10:26:59 -0500

    fixed an issue where layout was not defined

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const moment = require('moment'); // Ensure moment is required
+const expressEJSLayouts = require('express-ejs-layouts');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const moment = require('moment');
 const moment_tz = require('moment-timezone');
 const hotkeys = require('hotkeys-js');
-const passport = require('passport');
-const flash = require('connect-flash');
 const zipcodeToTimezone = require('zipcode-to-timezone');
-const Knex = require('knex');
-const ejs = require('ejs');
-const expressEJSLayouts = require('express-ejs-layouts'); // Ensure express-ejs-layouts is required
 
-const routes = require('./Routes/routes.js');
-app.use('/', routes);
-
+const app = express();
 const port = 3000;
-app.set('views', path.join(__dirname, '/views')); // Corrected path
+
+// Set view engine
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(expressEJSLayouts);
+app.set('layout', 'layout'); // Set the default layout
 
 // BodyParser
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Static Files
 app.use(express.static('public'));
+
+// Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
 
 // Passport
 app.use(passport.initialize());
@@ -41,24 +49,20 @@ app.use((req, res, next) => {
 // Locals - exposed to each views
 app.locals.moment = moment;
 app.locals.moment_tz = moment_tz;
-app.locals.hotkeys = hotkeys;  
+app.locals.hotkeys = hotkeys;
 app.locals.zipcodeToTimezone = zipcodeToTimezone;
-
-// Allow json body
-app.use(express.json());
 
 // Routes
 app.use('/', require('./Routes/routes.js'));
 
+// Error handling
 function errorHandler(err, req, res, next) {
     res.status(500);
     res.render('error', { title: 'An error has occurred', error: err });
 }
-
 app.use(errorHandler);
-console.log('Error handler added.');
-console.log('App started.');
-console.log('http://localhost:3000');
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
