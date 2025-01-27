413fc63be7684ee440f521f7d8ab57a5db7a0ed9
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-01-27 15:46:42 -0500

    Fixed the navigation

--- Controllers/dashboard.js
+++ Controllers/dashboard.js
@@ -6,7 +6,3 @@ exports.account = (req, res) => {
     const view = req.params.view || '1'; // Default to '0' if not provided
     res.render('patients/account', { view: view, title: 'Account' });
 }
-
-exports.diagnosis = (req, res) => {
-    res.render('patients/partials/diagnosis', { title: 'Diagnosis' });
-}
