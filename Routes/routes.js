ccb6817cf67e20bd6d6a8cb520ef9cc82918b829
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-02-24 21:03:23 -0500

    Created an insert query

--- Routes/routes.js
+++ Routes/routes.js
@@ -7,6 +7,8 @@ router.get('/home', Dashboard.index);
 router.get('/account/:patient_id/:view?', Dashboard.account);
 // router.get('/cs_rep', Dashboard.getAccounts);
 
+// router.post('/account/:patient_id', Dashboard.updateAccount);
+
 
 
 module.exports = router;
--- Services/queue.js
+++ Services/queue.js
@@ -15,5 +15,28 @@ module.exports = {
         .where({'patient_id' : patientId});
         console.log(data);
         return data[0];
-    }
+    },
+
+    // *******************************************************
+    // INSERT Queries
+    // *******************************************************
+
+    insertNewAccount: async (account, firstName, lastName, address1, address2, city, state, zip, phone1, phone2, email) => {
+        const data = await db('accounts')
+        .insert({
+            'account' : account,
+            'first_name' : firstName,
+            'last_name' : lastName,
+            'address1' : address1,
+            'address2' : address2,
+            'city' : city,
+            'state' : state,
+            'zip' : zip,
+            'phone1' : phone1,
+            'phone2' : phone2,
+            'email' : email
+        });
+        console.log(data);
+        return data;
+    },
 };
